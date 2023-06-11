b696bae0fd1926a82dd21eb60e5c15d9b1a55af2
NormanFebrio <110407530+NormanFebrio@users.noreply.github.com>
2023-06-11 18:17:43 +0700

    Update main.py
    add if __name__ == "__main__"

--- main.py
+++ main.py
@@ -18,3 +18,6 @@ async def predict_image(file: UploadFile = File(...)):
     prediction = predict(image)
 
     return prediction
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
